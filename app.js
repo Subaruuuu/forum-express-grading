8aea85187a58c0385f0ffd768debe75f93024c41
subaruuuu <meteorbs13@gmail.com>
2021-01-28 15:33:43 +0800

    feat: add signup algorithm & flash message

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
 const db = require('./models')
 const app = express()
 const port = 3000
@@ -7,6 +9,20 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+
+app.use(session({
+  secret: "forumSecret",
+  saveUninitialized: false,
+  resave: false
+}))
+
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync()
