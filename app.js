9d815dd49459c7341328d4ba3ef77551987a2f5b
subaruuuu <meteorbs13@gmail.com>
2021-01-28 18:47:46 +0800

    feat: add passport & user signin authentication

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('./config/passport')
 const db = require('./models')
 const app = express()
 const port = 3000
+
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -16,10 +18,12 @@ app.use(session({
   resave: false
 }))
 
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 
 app.use((req, res, next) => {
-  res.locals.success_msg = req.flash('success_msg')
+  res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
   next()
 })
@@ -29,6 +33,6 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
