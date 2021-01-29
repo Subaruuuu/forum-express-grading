170c1da39487169341aae3e25ffc261dfb5ea6b8
subaruuuu <meteorbs13@gmail.com>
2021-01-29 10:57:13 +0800

    feat: add create restaurant function

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,6 +7,24 @@ const adminController = {
       .then(restaurants => {
         return res.render('admin/restaurants', { restaurants: restaurants })
       })
+  },
+
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    const { name, tel, address, opening_hours, description } = req.body
+
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+    return Restaurant.create({ ...req.body })
+      .then((restaurant) => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
   }
 }
 
