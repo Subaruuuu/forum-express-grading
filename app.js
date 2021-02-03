d4910e83e46eb227e1e35e1647d86135867b3e37
subaruuuu <meteorbs13@gmail.com>
2021-02-03 08:17:46 +0800

    feat: Update category select in edit page using ifCond-helper

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  helpers: require('./config/handlebars-helpers')
+}))
+
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
