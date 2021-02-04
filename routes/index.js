2dceea88cbee84a330281108d0c789b98c1ca28c
subaruuuu <meteorbs13@gmail.com>
2021-02-04 10:49:05 +0800

    feat: Add admin categories delete feature

--- routes/index.js
+++ routes/index.js
@@ -30,17 +30,17 @@ module.exports = (app, passport) => {
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', authenticated, restController.getRestaurants)
 
-  //get, edit, delete
+  //users
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/categories'))
-
   app.get('/admin/users', authenticatedAdmin, adminController.getUsers)
   app.put('/admin/users/:id/toggleAdmin', authenticatedAdmin, adminController.toggleAdmin)
 
-  //get categories
+  //categories
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
   app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
   app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
   app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
+  app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
 
   //restaurants
   app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
