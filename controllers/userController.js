934da74206b89972445c182a4052c48de25e3c87
subaruuuu <meteorbs13@gmail.com>
2021-02-11 10:27:03 +0800

    fix typo link issue

--- controllers/userController.js
+++ controllers/userController.js
@@ -119,7 +119,7 @@ const userController = {
             .then(user => {
               return user.update({
                 name: req.body.name,
-                image: file ? img.data.link : restaurant.image
+                image: file ? img.data.link : user.image
               })
             })
             .then(user => {
